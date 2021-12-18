a0b25355a28a2cb7a74559cdeae302ae44005236
kota <52403688+yashikota@users.noreply.github.com>
2021-12-18 10:50:39 +0900

    readme 修正

--- sylabus/main.py
+++ sylabus/main.py
@@ -4,18 +4,20 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
+
 def export_list_csv(export_list, csv_dir):
 
     with open(csv_dir, "w") as f:
         writer = csv.writer(f, lineterminator='\n')
 
-        if isinstance(export_list[0], list): #多次元の場合
+        if isinstance(export_list[0], list):  # 多次元の場合
             writer.writerows(export_list)
 
         else:
             writer.writerow(export_list)
 
-csv_file = open("number.csv", "r", encoding="UTF-8", errors="", newline="" )
+
+csv_file = open("number.csv", "r", encoding="UTF-8", errors="", newline="")
 number_data = csv.reader(csv_file)
 
 ans = []
@@ -25,14 +27,15 @@ for i in number_data:
         number = str(j)
         print(number)
 
-        url = "https://www.portal.oit.ac.jp/CAMJWEB/slbssbdr.do?value(risyunen)=2021&value(semekikn)=1&value(kougicd)=" + number + "&value(crclumcd)=10201200"
+        url = "https://www.portal.oit.ac.jp/CAMJWEB/slbssbdr.do?value(risyunen)=2021&value(semekikn)=1&value(kougicd)=" + \
+            number + "&value(crclumcd)=10201200"
 
         #req = urllib.request.Request(url)
         #html = requests.get(url).text
-        path = "./html/"  + number + ".html"
-        localhtml = open(path, "r", encoding="UTF-8", errors="", newline="" )
+        path = "./html/" + number + ".html"
+        localhtml = open(path, "r", encoding="UTF-8", errors="", newline="")
         #soup=BeautifulSoup(html,"html.parser")
-        soup=BeautifulSoup(localhtml,"html.parser")
+        soup = BeautifulSoup(localhtml, "html.parser")
 
         elmes = soup.select('.kougi')
 
@@ -43,9 +46,9 @@ for i in number_data:
             #text = script.get_text()
             keystring += elem.get_text()
 
-        lines= [line.strip() for line in keystring.splitlines()]
+        lines = [line.strip() for line in keystring.splitlines()]
 
-        text=",".join(line for line in lines if line)
+        text = ",".join(line for line in lines if line)
 
         text = text.split(',')
 
@@ -58,7 +61,6 @@ for i in number_data:
         csvtext.append(text[5])
         csvtext.append(text[6].replace('\u3000', ''))
         csvtext.append(str(number))
-
 
         #print(csvtext)
 
