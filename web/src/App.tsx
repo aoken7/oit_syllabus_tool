76a15849a24835d01deebf3aaf5eed614c9a45e7
kota <52403688+yashikota@users.noreply.github.com>
2022-01-17 23:38:57 +0900

    manifest修正 使わないimport削除

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,17 @@
-import React, { useEffect, useState } from 'react';
 import { List } from "./components/List";
-import type { Syllabus } from "./types/syllabus";
-import data from "./data.json"
+import datas from "./data.json"
 
 export const App = () => {
   return (
     <div>
-      {data.map(datas => (
+      {datas.map(data => (
         <List
-          kougi={datas.kougi}
-          nenji={datas.nenji}
-          kikan={datas.kikan}
-          numbering={datas.numbering}
-          tantousya={datas.tantousya}
-          link={datas.link}
+          kougi={data.kougi}
+          nenji={data.nenji}
+          kikan={data.kikan}
+          numbering={data.numbering}
+          tantousya={data.tantousya}
+          link={data.link}
         />
       ))}
     </div>
