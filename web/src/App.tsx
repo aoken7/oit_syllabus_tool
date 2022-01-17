ce513856a80c629011bd47ca5e5ebfa8d8cdaab3
kota <52403688+yashikota@users.noreply.github.com>
2022-01-17 13:15:40 +0900

    jsonデータの表示

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { List } from "./components/List";
+import axios from "axios";
+import type { Syllabus } from "./types/syllabus";
 
-function App() {
+export const App = () => {
+  const [users, setUsers] = useState<Syllabus[]>([]);
+
+  useEffect(() => {
+    axios.get<Syllabus[]>("https://gist.githubusercontent.com/yashikota/1acd6ebfdcb9008af898ef9cb38f6782/raw/9534eb9c2aa2fec44211ccabfe592291e31f2994/oit").then((res) => {
+      setUsers(res.data);
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.map(user => (
+        <List
+          kougi={user.kougi}
+          nenji={user.nenji}
+          kikan={user.kikan}
+          numbering={user.numbering}
+          tantousya={user.tantousya}
+          link={user.link}
+        />
+      ))}
     </div>
   );
-}
-
-export default App;
+};
