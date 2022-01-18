c205db0743dea9d0e3c8167e6344a17ff0ee5865
kota <52403688+yashikota@users.noreply.github.com>
2022-01-18 16:39:55 +0900

    AppBar 仮想テーブル追加

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import SearchAppBar from './components/SearchBar';
+import ReactVirtualizedTable from './components/VirtualizedTable';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
+    <SearchAppBar />
+    <ReactVirtualizedTable />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
