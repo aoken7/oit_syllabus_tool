4a919de440ecc249797dd1c5d7510c73ab94511a
kota <52403688+yashikota@users.noreply.github.com>
2022-01-23 20:07:06 +0900

    画像変換完了

--- tool/timetable/pdf2png.py
+++ tool/timetable/pdf2png.py
@@ -1,21 +1,23 @@
 import os
 from pathlib import Path
 from pdf2image import convert_from_path
+import glob
 
 # poppler/binを環境変数PATHに追加する
 poppler_dir = Path(__file__).parent.absolute() / "poppler/bin"
 os.environ["PATH"] += os.pathsep + str(poppler_dir)
 
-# PDFファイルのパス
-pdf_path = Path("./2021/*.pdf")
+# PDFファイルのパスを取得し順番に捌いていく
+for x in glob.glob("./2021_pdf/*.pdf"):
+    pdf_path = Path(x)
 
-# PDF -> Image に変換
-pages = convert_from_path(str(pdf_path), 200)
+    # pdfから画像に変換
+    pages = convert_from_path(str(pdf_path), dpi=200)
 
-# 画像ファイルを１ページずつ保存
-image_dir = Path("./image_file")
-for i, page in enumerate(pages):
-    file_name = pdf_path.stem + "_{:02d}".format(i + 1) + ".png"
-    image_path = image_dir / file_name
-    # JPEGで保存
-    page.save(str(image_path), "PNG")
+    # 画像ファイルを１ページずつ保存
+    image_dir = Path("./2021_png")
+    for i, page in enumerate(pages):
+        file_name = f'{pdf_path.stem}_{i + 1}.png'
+        image_path = f'{image_dir}/{file_name}'
+        # PNGで保存
+        page.save(str(image_path), "PNG")
