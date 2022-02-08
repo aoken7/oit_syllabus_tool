d146d9c031f59e769ee5b9ee1865c19ebecd79b5
kota <52403688+yashikota@users.noreply.github.com>
2022-02-08 19:49:02 +0900

    import修正

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { Link as MuiLink } from '@mui/material';
+import { Link as MuiLink, styled } from '@mui/material';
 import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { styled } from '@mui/material';
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import Brightness2Icon from "@mui/icons-material/Brightness3";
 import IconButton from '@mui/material/IconButton/IconButton';
