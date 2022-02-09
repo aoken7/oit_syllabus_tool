e6620fc09f11c75ddb09cffe4a60556a53ca9488
kota <52403688+yashikota@users.noreply.github.com>
2022-02-09 12:39:51 +0900

    ファイル名変更 複数年度に対応

--- tool/extractor.py
+++ tool/extractor.py
@@ -2,20 +2,23 @@ import os
 from pathlib import Path
 from pdf2image import convert_from_path
 import glob
+from tqdm import tqdm
+
+year = '2021'
 
 # poppler/binを環境変数PATHに追加する
-poppler_dir = Path(__file__).parent.absolute() / "poppler/bin"
+poppler_dir = Path(__file__).parent.absolute() / "./timetable/poppler/bin"
 os.environ["PATH"] += os.pathsep + str(poppler_dir)
 
 # PDFファイルのパスを取得し順番に捌いていく
-for x in glob.glob("./2021/pdf/*.pdf"):
+for x in tqdm(glob.glob("./timetable/" + year + "/pdf/*.pdf")):
     pdf_path = Path(x)
 
     # pdfから画像に変換
     pages = convert_from_path(str(pdf_path), dpi=200)
 
     # 画像ファイルを１ページずつ保存
-    image_dir = Path("./2021/png")
+    image_dir = Path("./timetable/" + year + "/png")
     for i, page in enumerate(pages):
         file_name = f'{pdf_path.stem}_{i + 1}.png'
         image_path = f'{image_dir}/{file_name}'
