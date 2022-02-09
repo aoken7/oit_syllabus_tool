58562bde376765aba157fbb4054d75d3512e155d
kota <52403688+yashikota@users.noreply.github.com>
2022-02-09 15:16:52 +0900

    関数化

--- tool/extractor.py
+++ tool/extractor.py
@@ -4,18 +4,18 @@ from pdf2image import convert_from_path
 import glob
 from tqdm import tqdm
 
-year = '2021'
 
-# poppler/binを環境変数PATHに追加する
-poppler_dir = Path(__file__).parent.absolute() / "./timetable/poppler/bin"
-os.environ["PATH"] += os.pathsep + str(poppler_dir)
+def pdf2png(year: str):
+    # poppler/binを環境変数PATHに追加する
+    poppler_dir = Path(__file__).parent.absolute() / "./timetable/poppler/bin"
+    os.environ["PATH"] += os.pathsep + str(poppler_dir)
 
-# PDFファイルのパスを取得し順番に捌いていく
-for x in tqdm(glob.glob("./timetable/" + year + "/pdf/*.pdf")):
-    pdf_path = Path(x)
+    # PDFファイルのパスを取得し順番に捌いていく
+    for x in tqdm(glob.glob("./timetable/" + year + "/pdf/*.pdf")):
+        pdf_path = Path(x)
 
-    # pdfから画像に変換
-    pages = convert_from_path(str(pdf_path), dpi=200)
+        # pdfから画像に変換
+        pages = convert_from_path(str(pdf_path), dpi=200)
 
     # 画像ファイルを１ページずつ保存
     image_dir = Path("./timetable/" + year + "/png")
@@ -24,3 +24,21 @@ for x in tqdm(glob.glob("./timetable/" + year + "/pdf/*.pdf")):
         image_path = f'{image_dir}/{file_name}'
         # PNGで保存
         page.save(str(image_path), "PNG")
+
+
+# def png2num(year: str):
+#     pass
+
+# def num2csv(year: str):
+#     pass
+
+
+def main():
+    year = '2021'
+    pdf2png(year)
+#    png2num(year)
+#    num2csv(year)
+
+
+if __name__ == "__main__":
+    main()
