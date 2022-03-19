6c498535eef55eca17e20c7389a6ad5590c1ffc8
GenichiMaruo <gen1010f8e7@gmail.com>
2022-03-20 01:43:41 +0900

    scraping_syllabus関数の改変

--- tool/scraping.py
+++ tool/scraping.py
@@ -57,7 +57,7 @@ class SyllabusTool:
             syllabus_dict = dict()
         return syllabus_dict
 
-    def scraping_syllabus(self, number: str, year: str, csv: str, syllabus_dict_list: list) -> Dict[str, str]:
+    def scraping_syllabus(self, number: str, year: str, csv: str):
         url = "https://www.portal.oit.ac.jp/CAMJWEB/slbssbdr.do?value(risyunen)=" + year + "&value(semekikn)=1&value(kougicd)=" + \
             number + "&value(crclumcd)=10201200"
 
@@ -71,33 +71,26 @@ class SyllabusTool:
 
         syllabus_dict = self.extract_element(html)
         key_list = (list(syllabus_dict.keys()))
-        if syllabus_dict is not None and key_list != []:
-            syllabus_dict_list, syllabus_dict = self.duplicate_check(
-                syllabus_dict, number, syllabus_dict_list)
-            if syllabus_dict is not None:
-                syllabus_dict["link"] = url
-                syllabus_dict["numbering"] = number
-                syllabus_dict["gakka"] = csv[0]
-                return syllabus_dict
-            else:
-                return None
-        else:
+        if key_list != [] and self.duplicate_check(syllabus_dict, number):
             syllabus_dict["link"] = url
             syllabus_dict["numbering"] = number
             syllabus_dict["gakka"] = csv[0]
-            return syllabus_dict
+            self.syllabus_dict_list.append(syllabus_dict) # リストに追加
+        else:
+            # ページがない時のエラー処理，もう少し上手くやりたい
+            self.error += 1  # エラー数をカウント
 
-    def duplicate_check(self, check_syllabus_dict: list, number: str, syllabus_dict_list: list):
-        for i in range(len(syllabus_dict_list)):
-            syllabus_dict = syllabus_dict_list[i]
+    def duplicate_check(self, check_syllabus_dict: list, number: str):
+        for i in range(len(self.syllabus_dict_list)):
+            syllabus_dict = self.syllabus_dict_list[i]
             if syllabus_dict["kougi"] == check_syllabus_dict["kougi"] and \
                syllabus_dict["nenji"] == check_syllabus_dict["nenji"] and \
                syllabus_dict["kikan"] == check_syllabus_dict["kikan"] and \
                syllabus_dict["tantousya"] == check_syllabus_dict["tantousya"] and \
                syllabus_dict["tani"] == check_syllabus_dict["tani"]:
-                syllabus_dict_list[i]["numbering"] += " , " + number
-                return syllabus_dict_list, None
-        return syllabus_dict_list, check_syllabus_dict
+                self.syllabus_dict_list[i]["numbering"] += " , " + number
+                return False
+        return True
 
     def make_syllabus_dict_list(self):
         duplicate_check = list()
@@ -109,15 +102,7 @@ class SyllabusTool:
             numbers.sort()
             duplicate_check.sort()
             for number in tqdm(numbers, desc=csv):
-                syllabus_dict = self.scraping_syllabus(
-                    number, self.year, csv, self.syllabus_dict_list)
-                if syllabus_dict is None:
-                    continue
-                # ページがない時のエラー処理，もう少し上手くやりたい
-                if len(syllabus_dict) < 4:
-                    self.error += 1  # エラー数をカウント
-                    continue
-                self.syllabus_dict_list.append(syllabus_dict)  # リストに追加
+                self.scraping_syllabus(number, self.year, csv)
         return duplicate_check
 
     def main(self, *args):
