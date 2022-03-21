06625185549a75e69073f078496d3679d8bbdd75
kota <52403688+yashikota@users.noreply.github.com>
2022-03-21 16:21:18 +0900

    2021を削除

--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -2,7 +2,6 @@ import MaterialTableCore from '@material-table/core';
 import { Button, Select, MenuItem, FormControl } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import React, { forwardRef } from 'react';
-import data2021 from '../data/2021.json';
 import data2022 from '../data/2022.json';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
@@ -33,11 +32,11 @@ const useStyles = makeStyles((theme) => ({
 
 export const Table = (props: any) => {
     let data = data2022;
-    if (props.year === '2021') {
-        data = data2021;
-    } else {
-        data = data2022;
-    }
+    // if (props.year === '2021') {
+    //     data = data2021;
+    // } else {
+    //     data = data2022;
+    // }
 
     const classes = useStyles();
 
